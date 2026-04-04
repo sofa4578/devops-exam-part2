34e316527dff57dc16ce3108a9e011244a00269c
Sofia Levko <113248354+sofa4578@users.noreply.github.com>
2026-04-04 23:49:27 +0300

    Update provider.tf

--- task1/provider.tf
+++ task1/provider.tf
@@ -16,11 +16,10 @@ terraform {
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_metadata_api_check     = true
-    skip_requesting_account_id  = true  # Ось ця стрічка виправляє головну помилку
-    skip_s3_checksum            = true  # Ця стрічка теж важлива для DO Spaces
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true 
   }
 }
 
 provider "digitalocean" {
-  # Токен буде братися з системної змінної DIGITALOCEAN_TOKEN
 }
