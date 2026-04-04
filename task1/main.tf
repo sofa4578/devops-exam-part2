3360964a1f828b8383cd87831c75cb9477b1f8e9
Sofia Levko <113248354+sofa4578@users.noreply.github.com>
2026-04-04 23:48:52 +0300

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -1,38 +1,31 @@
-# 1. Віртуальна приватна хмара (VPC)
 resource "digitalocean_vpc" "vpc" {
   name     = "levko-vpc"
-  region   = "fra1" # Франкфурт - найближче до України
+  region   = "fra1"
   ip_range = "10.10.10.0/24"
 }
 
-# 2. Сховище для обʼєктів (Бакет)
 resource "digitalocean_spaces_bucket" "bucket" {
-  name   = "levko-bucket-${random_id.bucket_suffix.hex}" # Додаємо суфікс, бо імена бакетів мають бути унікальними в усьому світі
+  name   = "levko-bucket-${random_id.bucket_suffix.hex}"
   region = "fra1"
 }
 
-# Генерація випадкового суфікса для унікальності бакета
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-# 3. Віртуальна машина (Droplet)
 resource "digitalocean_droplet" "node" {
   name     = "levko-node"
   region   = "fra1"
-  size     = "s-2vcpu-4gb" # Вимоги Minikube (мінімум 2 CPU, 2+ GB RAM)
-  image    = "ubuntu-24-04-x64" # Образ ОС Ubuntu 24
+  size     = "s-2vcpu-4gb"
+  image    = "ubuntu-24-04-x64"
   vpc_uuid = digitalocean_vpc.vpc.id
 }
 
-# 4. Налаштування фаєрволу
 resource "digitalocean_firewall" "firewall" {
   name = "levko-firewall"
 
-  # Прив'язуємо фаєрвол до нашої ВМ
   droplet_ids =[digitalocean_droplet.node.id]
 
-  # Вхідні підключення
   dynamic "inbound_rule" {
     for_each =["22", "80", "443", "8000", "8001", "8002", "8003"]
     content {
@@ -42,7 +35,6 @@ resource "digitalocean_firewall" "firewall" {
     }
   }
 
-  # Вихідні підключення (1-65535) - дозволяємо TCP та UDP
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
